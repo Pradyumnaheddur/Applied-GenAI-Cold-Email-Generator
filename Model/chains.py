f41bedafe7f86a51a7a761d8d52ac41637ea7246
Pradyumna Heddur Nagendra <pradyumnaheddur@gmail.com>
2024-09-30 11:58:15 +0200

    Update chains.py

--- Model/chains.py
+++ Model/chains.py
@@ -18,7 +18,8 @@ class Chain:
             {page_data}
             ### INSTRUCTION:
             The scraped text is from the career's page of a website.
-            Your job is to extract the job postings and return them in JSON format containing the following keys: `role`, `experience`, `skills` and `description`.
+            Your job is to extract the job postings and return them in JSON format containing the following keys: 
+            `role`, `experience`, `skills` and `description`.
             Only return the valid JSON.
             ### VALID JSON (NO PREAMBLE):
             """
